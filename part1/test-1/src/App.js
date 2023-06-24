82eb46da27509bd6bbeb1d9b931f7c089c7e95ae
wannalina <annalina_wheeler@hotmail.com>
2023-06-24 16:42:54 +0300

    part1 test-1: made changes according to teaching material and created new react app for exercise

--- part1/test-1/src/App.js
+++ part1/test-1/src/App.js
@@ -8,18 +8,25 @@ const Hello = (props) => {
 
 const App = () => {
   const friends = [
-    {name:'Peter', age:23},
-    {name:'Isla', age:11},
+    { name:'Peter', age:23 },
+    { name:'Isla', age:11 },
   ];
+  const more_friends = ['Maya', 'Claire'];
 
   return (
     // can either be wrapped in <div></div> or fragment like this
-  <> 
-    <h1>Greetings</h1>
-    <Hello name='George' age='23' />
-    <Hello name={friends[0].name} age={friends[0].age} />
-    <Hello name={friends[1].name} age={friends[1].age} />
-  </>
+    <div>
+      <div> 
+        <h1>Greetings</h1>
+        <Hello name='George' age='23' />
+        <Hello name={friends[0].name} age={friends[0].age} />
+        <Hello name={friends[1].name} age={friends[1].age} />
+      </div>
+
+      <div>
+        <p>{more_friends}</p>
+      </div>
+    </div>
   )
 }
 
