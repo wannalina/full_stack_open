1be6ee564e66197a7b16d119c50832de36476c54
wannalina <annalina_wheeler@hotmail.com>
2023-07-02 10:21:04 +0300

    part1 exercise 1.1: created Header, Content and Total components and refactored the code accordingly

--- part1/course-information-app/src/App.js
+++ part1/course-information-app/src/App.js
@@ -1,26 +1,50 @@
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
+const Header = (course) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Content = (course) => {
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
+  const exercises3 = 14
+
+  return ( 
+    <>
+      <p>
+        {course.part1} {exercises1}
+      </p><p>
+        {course.part2} {exercises2}
+      </p><p>
+        {course.part3} {exercises3}
+      </p>
+    </>
+  )
+}
+
+const Total = (course) => {
+  const total_number = course.exercises1 + course.exercises2 + course.exercises3
+  
+  return (
+    <p>Number of exercises {total_number}</p>
+  )
+}
+
+const App = () => {
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
   const part3 = 'State of a component'
+
+  const exercises1 = 10
+  const exercises2 = 7
   const exercises3 = 14
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name = {'Half Stack application development'}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
 }
