f8139f75785c126bf5bb5b98ef3bd2eaf7a3b66c
wannalina <annalina_wheeler@hotmail.com>
2023-08-10 20:24:50 +0300

    part 1 exercise 1.4: placed objects into array & made necessary changes to components

--- part1/course-information-app/src/App.js
+++ part1/course-information-app/src/App.js
@@ -1,61 +1,58 @@
 
-const Header = (course) => {
+const Header = ({course}) => {
   return (
-    <h1>{course.name}</h1>
+    <h1>{course}</h1>
   )
 }
 
-const Content = (course) => { 
-
+const Content = ({parts}) => { 
   return ( 
     <div>
-      <Part content={course.part1_content}/>
-      <Part content={course.part2_content}/>
-      <Part content={course.part3_content}/>
+      <Part name={parts[0].name} exercises={parts[0].exercises}/>
+      <Part name={parts[1].name} exercises={parts[1].exercises}/>
+      <Part name={parts[2].name} exercises={parts[2].exercises}/>
     </div>
-  )
+  );
 }
 
-const Total = (course) => {
-  const total_number = course.exercises1 + course.exercises2 + course.exercises3
-  
+const Total = ({parts}) => {
+  const total_number = parts[0].exercises + parts[1].exercises + parts[2].exercises
   return (
-    <p>Number of exercises {total_number}</p>
+    <p>Number of exercises: {total_number}</p>
   )
 }
 
-const Part = (course) => {
+const Part = ({name, exercises}) => {
     return (
       <p>
-        {course.content}
+        {name}: {exercises}
         <br />
       </p>
     )
 }
 
 const App = () => {
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header name = {'Half Stack application development'}/>
-      <Content 
-        part1_content={`${part1.name} ${part1.exercises}`}  
-        part2_content={`${part2.name} ${part2.exercises}`}
-        part3_content={`${part3.name} ${part3.exercises}`}/>
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+      <Header course = {course}/>
+      <Content parts = {parts}/>
+      <Total parts = {parts}/>
     </div>
   )
 }
