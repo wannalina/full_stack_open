092011f4e077be11bc75658fc35d2f96d0534d57
wannalina <annalina_wheeler@hotmail.com>
2023-08-10 20:30:50 +0300

    part 1 exercise 1.5: made course and its parts one object and refactored everything else accordingly

--- part1/course-information-app/src/App.js
+++ part1/course-information-app/src/App.js
@@ -1,7 +1,7 @@
 
-const Header = ({course}) => {
+const Header = ({name}) => {
   return (
-    <h1>{course}</h1>
+    <h1>{name}</h1>
   )
 }
 
@@ -32,27 +32,29 @@ const Part = ({name, exercises}) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [ 
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+          name: 'Using props to pass data',
+          exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content parts = {parts}/>
-      <Total parts = {parts}/>
+      <Header name = {course.name}/>
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
     </div>
   )
 }
